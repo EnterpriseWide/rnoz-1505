6711a598ede1cd3e9dd4131baf31cc5bc95e2aff
Ben Norman <support@ewide.biz>
2015-05-20 08:54:28 +1000

    moved error implementation to the base controller

--- wwwroot/Controllers/AccountController.cs
+++ wwwroot/Controllers/AccountController.cs
@@ -358,35 +358,6 @@ namespace ewide.web.Controllers
             get { return Request.GetOwinContext().Authentication; }
         }
 
-        private IHttpActionResult GetErrorResult(IdentityResult result)
-        {
-            if (result == null)
-            {
-                return InternalServerError();
-            }
-
-            if (!result.Succeeded)
-            {
-                if (result.Errors != null)
-                {
-                    foreach (string error in result.Errors)
-                    {
-                        ModelState.AddModelError("", error);
-                    }
-                }
-
-                if (ModelState.IsValid)
-                {
-                    // No ModelState errors are available to send, so just return an empty BadRequest.
-                    return BadRequest();
-                }
-
-                return BadRequest(ModelState);
-            }
-
-            return null;
-        }
-
         private class ExternalLoginData
         {
             public string LoginProvider { get; set; }
