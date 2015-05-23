38d3e69df08f4abe853d1dd99d3a888a2a160003
Ben Norman <support@ewide.biz>
2015-05-23 14:06:53 +1000

    added download button for the Learning Plan PDF to the view

--- data/rightNow/src/client/app/learningplan/learningplan.controller.js
+++ data/rightNow/src/client/app/learningplan/learningplan.controller.js
@@ -4,15 +4,15 @@
     angular
         .module('app.learningplan')
         .controller('LearningPlanController', LearningPlanController);
+    LearningPlanController.$inject = ['logger', '$stateParams', '$q', 'dataservice', 'authservice'];
 
-    LearningPlanController.$inject = ['logger', '$stateParams', '$q', 'dataservice'];
-
-    /* @ngInject */
-    function LearningPlanController(logger, $stateParams, $q, dataservice) {
+    function LearningPlanController(logger, $stateParams, $q, dataservice, authservice) {
         var vm = this;
         vm.title = 'Learning Plan';
         vm.data = [];
+        vm.authData = authservice.authData;
         vm.save = save;
+        vm.apiurl = dataservice.apiurl;
 
         activate();
 
