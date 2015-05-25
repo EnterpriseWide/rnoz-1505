78cb29c55ec655114edae9ecbf6b0fd839cda06f
Ben Norman <support@ewide.biz>
2015-05-25 12:35:32 +1000

    turned on automatic migrations

--- wwwroot/Migrations/Configuration.cs
+++ wwwroot/Migrations/Configuration.cs
@@ -12,7 +12,7 @@ namespace ewide.web.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
             AutomaticMigrationDataLossAllowed = true;
         }
 
