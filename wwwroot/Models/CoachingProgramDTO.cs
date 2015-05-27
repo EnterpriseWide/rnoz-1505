0b2b92b728fd72f1cc094defb3e3b92cfd0de9f3
Ben Norman <support@ewide.biz>
2015-05-28 09:15:10 +1000

    update to client on server side

---
+++ wwwroot/Models/CoachingProgramDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ewide.web.Models
+{
+    public class CoachingProgramDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        public DateTime UpdatedAt { get; set; }
+        [Required]
+        public virtual ApplicationUser Coach { get; set; }
+        [Required]
+        public virtual ApplicationUser Coachee { get; set; }
+        public ICollection<CoachingSession> CoachingSessions { get; set; }
+        public int AssignmentCount { get; set; }
+    }
+}
