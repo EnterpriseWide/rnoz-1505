4d2cf6e05f8b093a2365f0e5c05ff088722b9df6
Ben Norman <support@ewide.biz>
2015-06-10 23:27:50 +1000

    added login-page class to body on LoginPage

--- data/rightNow/src/client/app/app.module.js
+++ data/rightNow/src/client/app/app.module.js
@@ -1,19 +1,32 @@
 (function () {
     'use strict';
 
-    angular.module('app', [
-        'app.assignment',
-        'app.admin',
-        'app.core',
-        'app.help',
-        'app.layout',
-        'app.learningplan',
-        'app.login',
-        'app.profile',
-        'app.program',
-        'app.resource',
-        'app.upload',
-        'app.widgets'
-    ]);
+    angular
+        .module('app', [
+            'app.assignment',
+            'app.admin',
+            'app.core',
+            'app.help',
+            'app.layout',
+            'app.learningplan',
+            'app.login',
+            'app.profile',
+            'app.program',
+            'app.resource',
+            'app.upload',
+            'app.widgets',
+            'app.yourcoach'
+        ])
+        .run(appRun);
+    appRun.$inject = ['$rootScope', 'logger'];
+    function appRun($rootScope, logger) {
+        $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+            if (toState.controller == "LoginController") {
+                $rootScope.bodyClass = "login-page";
+            } else {
+                $rootScope.bodyClass = "";
+            }
+        });
+    }
 
 })();
