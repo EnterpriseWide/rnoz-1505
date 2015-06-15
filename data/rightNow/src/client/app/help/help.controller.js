f81ed3e720a733145456585cdc25af455504147c
Ben Norman <support@ewide.biz>
2015-06-15 12:56:01 +1000

    iscoachee on help and profile now have back to dashboard link to program page and if coach and admin goes to programs

--- data/rightNow/src/client/app/help/help.controller.js
+++ data/rightNow/src/client/app/help/help.controller.js
@@ -5,15 +5,27 @@
         .module('app.help')
         .controller('HelpController', HelpController);
 
-    HelpController.$inject = ['logger', 'authservice'];
+    HelpController.$inject = ['logger', 'authservice', 'dataservice'];
 
     /* @ngInject */
-    function HelpController(logger, authservice) {
+    function HelpController(logger, authservice, dataservice) {
         var vm = this;
         vm.title = 'Help';
         vm.authData = authservice.authData;
         vm.bodyText = '<p>Contact Info</p><p>PDF Instructions</p>' +
             '<p>And a few paragraphs of basic instructions</p>';
+        vm.authData = authservice.authData;
 
+        activate();
+
+        function activate() {
+            if (!(vm.authData.isCoach || vm.authData.isAdmin)) {
+                dataservice.listPrograms().then(function (data) {
+                    var program = data[0];
+                    vm.programId = program.Id;
+                });
+            }
+
+        }
     }
 })();
