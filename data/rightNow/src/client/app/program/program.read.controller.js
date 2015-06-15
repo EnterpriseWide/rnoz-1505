0106030e9a360687901bde4cb07b3e662f5f2b6d
Jonathon Kingston <jono@muchmedia.com.au>
2015-06-15 16:24:50 +0930

    A few more UI tweaks

--- data/rightNow/src/client/app/program/program.read.controller.js
+++ data/rightNow/src/client/app/program/program.read.controller.js
@@ -17,14 +17,14 @@
                 'message</p>',
             bodyTextYourCoachee: '<p>Read more information about your coachee, or send them a ' +
                 'message</p>',
-            bodyTextLearningPlan: '<p>This document is created over teh course of your coaching ' +
-                'program - both you and your coach can edit it, export it or email a copy.</p>',
+            bodyTextLearningPlan: '<p>This document is created over the course of your coaching ' +
+                'program - both you and your coach can edit it. You can also export it as a PDF or email it to another person.</p>',
             bodyTextAssignments: '<p>These assignments are specified by your coach and are ' +
                 'updated over the course of your coaching program.</p>',
-            bodyTextResources: '<p>Read more information about Resources here</p>',
-            bodyTextUploads: '<p>Read more information about Uploads here</p>',
+            bodyTextResources: '<p>Resources are files and links that have been added by your coach.</p>',
+            bodyTextUploads: '<p>Uploads are files and links that are added by you for your coach to view.</p>',
             bodyTextSessions: '<p>Read more information about Sessionshere</p>',
-            bodyTextSurveys: '<p>Read more information about surveys here</p>'
+            bodyTextSurveys: '<p>More text explaining surveys to go here.</p>'
         };
 
         activate();
@@ -60,7 +60,7 @@
 
         function sessionInfoReadMore(ev) {
             ngDialog.open({
-                template: '<div class="content-text-area"><h1>Coaching Sessions</h1><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p><p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p><p>Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.</p></div>' +
+                template: '<div class="content-text-area"><h1><span class="icon icon-forum"></span>&nbsp;Coaching Sessions</h1><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p><p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p><p>Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.</p></div>' +
                     '<div class="ngdialog-buttons">' +
                         '<button type="button" class="btn btn-blue" ng-click="closeThisDialog(0)">Close</button>' +
                     '</div>',
