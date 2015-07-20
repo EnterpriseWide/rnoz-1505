2324a72505a9995f2ba6ee87ceee536cd0cc4e42
Ben Norman <support@ewide.biz>
2015-07-21 02:28:58 +1000

    added user list for admin

--- wwwroot/Controllers/UsersController.cs
+++ wwwroot/Controllers/UsersController.cs
@@ -15,8 +15,47 @@ using System.Web.Http.Description;
 namespace ewide.web.Controllers
 {
     [Authorize(Roles = "Admin")]
+    [RoutePrefix("api/users")]
     public class UsersController : BaseApiController
     {
+        [ResponseType(typeof(GetUserForAdminResponse))]
+        [Route("ForAdmin")]
+        public IHttpActionResult GetUsersForAdmin(int pageNumber = 1, int pageSize = 25, String sort = "CreatedAt desc")
+        {
+            var currentUser = AppUserManager.FindById(User.Identity.GetUserId());
+            var coachRole = AppRoleManager.FindByName("Coach");
+            var users = (IQueryable<ApplicationUser>)AppUserManager.Users;
+            if (String.IsNullOrEmpty(sort) || sort == "null")
+            {
+                users = users.OrderBy(i => i.UserName);
+            }
+            else
+            {
+                if (sort.EndsWith(","))
+                {
+                    sort = sort.TrimEnd(',');
+                }
+                users = users.OrderBy(sort);
+            }
+            var count = users.Count();
+            users = users
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize);
+            return Ok(new GetUserForAdminResponse
+            {
+                TotalItems = count,
+                Items = users
+                    .Select(i => new UserHttpResponse
+                    {
+                        Id = i.Id,
+                        FirstName = i.FirstName,
+                        LastName = i.LastName,
+                        IsCoach = i.Roles.Any(j => j.RoleId == coachRole.Id),
+                    })
+                    .ToList(),
+            }); 
+        }
+
         public IEnumerable<UserHttpResponse> GetUsers()
         {
             var currentUser = AppUserManager.FindById(User.Identity.GetUserId());
@@ -32,6 +71,7 @@ namespace ewide.web.Controllers
                 .ToList();
             return users;
         }
+
     }
 
     public class UserHttpResponse
@@ -41,4 +81,11 @@ namespace ewide.web.Controllers
         public string FirstName { get; set; }
         public string LastName { get; set; }
     }
+
+    public class GetUserForAdminResponse
+    {
+        public int TotalItems { get; set; }
+        public List<UserHttpResponse> Items { get; set; }
+    }
+
 }
