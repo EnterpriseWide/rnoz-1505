0b12a230df70dcb0ccaacc04065f47928e7d7fdc
Ben Norman <support@ewide.biz>
2015-07-31 20:58:12 +1000

    setting the finishtime on the program list page

--- data/rightNow/src/client/app/program/program.list.controller.js
+++ data/rightNow/src/client/app/program/program.list.controller.js
@@ -11,6 +11,7 @@
         vm.programs = [];
         vm.sessions = [];
         vm.title = 'Coaching Programs';
+        vm.finishTime = finishTime;
 
         activate();
 
@@ -18,6 +19,11 @@
             var promises = [listPrograms(), listSessions()];
             return $q.all(promises).then(function() {
             });
+        }
+
+        function finishTime (session) {
+            var finishedAt = moment(session.StartedAt);
+            return finishedAt.add(session.Duration, 'm').toDate();
         }
 
         function listSessions() {
