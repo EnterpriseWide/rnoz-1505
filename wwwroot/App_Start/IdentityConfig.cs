712562b7537ba0f0365ff71c5cadfc7eda4bf7a5
Ben Norman <support@ewide.biz>
2015-07-31 20:58:43 +1000

    reduce password restrictions

--- wwwroot/App_Start/IdentityConfig.cs
+++ wwwroot/App_Start/IdentityConfig.cs
@@ -29,10 +29,10 @@ namespace ewide.web
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
                 RequireLowercase = true,
-                RequireUppercase = true,
+                RequireUppercase = false,
             };
             var dataProtectionProvider = options.DataProtectionProvider;
             if (dataProtectionProvider != null)
